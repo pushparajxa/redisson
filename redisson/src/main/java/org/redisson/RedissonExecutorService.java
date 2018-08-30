db804dcda64a9e7fc87eab782ba614ab613a230d
Pushparaj Motamari <pushparaj.motamari@rivigo.com>
2018-08-30 16:19:20 +0530

    redisson/src/main/java/org/redisson/RedissonExecutorService.java

--- redisson/src/main/java/org/redisson/RedissonExecutorService.java
+++ redisson/src/main/java/org/redisson/RedissonExecutorService.java
@@ -99,7 +99,8 @@ public class RedissonExecutorService implements RScheduledExecutorService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedissonExecutorService.class);
     
-    private static final RemoteInvocationOptions RESULT_OPTIONS = RemoteInvocationOptions.defaults().noAck().expectResultWithin(1, TimeUnit.HOURS);
+    private static final RemoteInvocationOptions RESULT_OPTIONS = RemoteInvocationOptions
+        .defaults().noAck().expectResultWithin(7, TimeUnit.DAYS);
     
     public static final int SHUTDOWN_STATE = 1;
     public static final int TERMINATED_STATE = 2;
